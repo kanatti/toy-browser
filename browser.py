af5d5676b74f09418e84c9a42867f81c61354501
Balagopal Kanattil <balukanattil@gmail.com>
2022-10-08 21:32:10 -0400

    Support file scheme

--- browser.py
+++ browser.py
@@ -2,43 +2,56 @@ import socket
 import ssl
 import sys
 
+class URL:
+    def __init__(self, raw_url: str):
+        scheme, url = raw_url.split("://", 1)
+        assert scheme in ["file", "http", "https"], "Unknown scheme {}".format(scheme)
 
-def request(url):
-    scheme, url = url.split("://", 1)
-    assert scheme in ["http", "https"], "Unknown scheme {}".format(scheme)
+        host, path = url.split("/", 1)
+        path = "/" + path
+        port = 80 if scheme == "http" else 443
 
-    host, path = url.split("/", 1)
-    path = "/" + path
 
+        if ":" in host:
+            host, port = host.split(":", 1)
+            port = int(port)
+
+        self.scheme = scheme
+        self.host = host
+        self.path = path
+        self.port = port
+        
+
+
+def request_file(url: URL):
+    with open(url.path, "r") as f:
+        body = f.read()
+    return {}, body
+
+def request_remote(url: URL):
     s = socket.socket(
         family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
     )
 
-    port = 80 if scheme == "http" else 443
-
-    if ":" in host:
-        host, port = host.split(":", 1)
-        port = int(port)
-
-    if scheme == "https":
+    if url.scheme == "https":
         ctx = ssl.create_default_context()
-        s = ctx.wrap_socket(s, server_hostname=host)
+        s = ctx.wrap_socket(s, server_hostname=url.host)
 
-    s.connect((host, port))
+    s.connect((url.host, url.port))
 
-    headers = {"Host": host, "Connection": "close", "User-Agent": "toybrowser"}
-    request_body = "GET {} HTTP/1.1\r\n".format(path).encode("utf8")
-    for key in headers:
-        request_body = request_body + "{}: {}\r\n".format(key, headers[key]).encode(
+    req_headers = {"Host": url.host, "Connection": "close", "User-Agent": "toybrowser"}
+    req_body = "GET {} HTTP/1.1\r\n".format(url.path).encode("utf8")
+    for key in req_headers:
+        req_body = req_body + "{}: {}\r\n".format(key, req_headers[key]).encode(
             "utf8"
         )
 
-    request_body = request_body + "\r\n".encode("utf8")
+    req_body = req_body + "\r\n".encode("utf8")
 
     print("making request")
-    print(request_body.decode("utf8"))
+    print(req_body.decode("utf8"))
 
-    s.send(request_body)
+    s.send(req_body)
 
     response = s.makefile("r", encoding="utf8", newline="\r\n")
 
@@ -62,10 +75,15 @@ def request(url):
 
     return headers, body
 
+def request(url: URL):
+    if url.scheme == "file":
+        return request_file(url)
+    else:
+        return request_remote(url)
 
-def print_content(body: str):
+def print_html(html: str):
     inside_tag = False
-    for ch in body:
+    for ch in html:
         if ch == "<":
             inside_tag = True
         elif ch == ">":
@@ -75,6 +93,7 @@ def print_content(body: str):
 
 
 if __name__ == "__main__":
-    headers, body = request(sys.argv[1])
+    url = URL(sys.argv[1])
+    headers, body = request(url)
     print(headers)
-    print_content(body)
+    print_html(body)
